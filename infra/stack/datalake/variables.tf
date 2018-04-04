3f9dc35d13376abe62be405771244f21522263ee
Sergio Dos Santos <sergiords@users.noreply.github.com>
2018-04-04 23:15:35 +0200

    rearrange variables

--- infra/stack/datalake/variables.tf
+++ infra/stack/datalake/variables.tf
@@ -1,15 +1,20 @@
-variable "region" {}
+variable "stage" {}
+variable "stage_tags" {
+    type = "map"
+}
 
 variable "project" {}
 variable "project_tags" {
     type = "map"
 }
 
-variable "stage" {}
-variable "stage_tags" {
-    type = "map"
+variable "stack" {
+    default = "datalake"
 }
 
 locals {
-    tags = "${merge(var.project_tags, var.stage_tags)}"
+    name = "${var.stage}-${var.project}-${var.stack}"
+    tags = "${merge(var.stage_tags, var.project_tags, map("Name", "${local.name}"))}"
 }
+
+variable "region" {}

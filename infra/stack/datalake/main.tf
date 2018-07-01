0eefc6604284d274f2580b96b3a36a3f412e17ed
Sergio Dos Santos <sergiords@users.noreply.github.com>
2018-07-01 23:23:43 +0200

    add datalake raw location prefix with a readme object

--- infra/stack/datalake/main.tf
+++ infra/stack/datalake/main.tf
@@ -10,3 +10,15 @@ resource "aws_s3_bucket" "datalake" {
     bucket = "${local.name}.${var.bucket_name_suffix}"
     tags = "${local.tags}"
 }
+
+resource "aws_s3_bucket_object" "raw" {
+    bucket = "${aws_s3_bucket.datalake.bucket}"
+    key = "/raw/readme.txt"
+    content_type = "text/plain"
+    content = <<EOF
+# RAW
+This bucket location should only contain raw data extracts organised by extraction date.
+- /raw/{source}/{year}/{month}/{day}/extract.json
+- ...
+EOF
+}
